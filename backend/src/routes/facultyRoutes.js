3f6a68b368c4e739a498afd2ccccbb2098476857
Ashlesha1611 <168618944+Ashlesha1611@users.noreply.github.com>
2026-02-25 01:15:07 +0530

    completed upload proof endpoint

--- backend/src/routes/facultyRoutes.js
+++ backend/src/routes/facultyRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { facultyController } from '../controllers/facultyController.js';
 import {authenticate} from '../middleware/authMiddleware.js';
+import { upload } from '../middleware/upload.js';
 
 const router = express.Router();
 
@@ -10,7 +11,19 @@ router.get('/:module/:id', authenticate, facultyController.getOne);
 router.put('/:module/:id', authenticate, facultyController.update);
 router.delete('/:module/:id', authenticate, facultyController.delete);
 
-router.post('/:module/:id/upload-proof', authenticate, facultyController.uploadProof);
+//router.post('/:module/:id/upload-proof', authenticate, facultyController.uploadProof);
+router.post(
+  '/:module/:id/proof',
+  authenticate,
+  upload.single('file'),
+  facultyController.uploadProof
+);
+router.get(
+  '/:module/:id/proof',
+  authenticate,
+  facultyController.getProof
+);
+
 router.post('/:module/validate', authenticate, facultyController.validateDuplicate);
 
 export default router;
